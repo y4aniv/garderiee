1dbbed338d13eb29c6c79efacfc3c0c3ff540e9c
Yaniv <120222659+y4aniv@users.noreply.github.com>
2023-05-21 01:18:43 +0000

    Envoie du mail avec Gmail

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 var colors = require('colors');
 var xlsx = require('node-xlsx').default;
 var asciiTable = require('ascii-table');
-var fs = require('fs');
 var pdf = require("pdf-creator-node")
+var keypress = require('keypress');
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'email@example.com',
+    pass: 'ultrasecretpwd'
+  }
+})
+
+keypress(process.stdin);
+
+var args = process.argv.slice(2);
+
+var options = {
+  ListeInscriptionGarderieXLSX_Path: args[0],
+  ListeEleveTotalXLSX_Path: args[1],
+}
 
 console.clear();
 
@@ -19,7 +37,7 @@ console.log(`> 6/8 rue des Bluets 75011`.blue)
 console.log("")
 
 console.log(`[+] Récupération de la liste d'élèves inscrits à la garderie`.green)
-var ListeInscriptionGarderieXLSX = xlsx.parse(`${__dirname}/data/Liste complète garderie 2022-2023.xlsx`)[0].data
+var ListeInscriptionGarderieXLSX = xlsx.parse(`${__dirname}/data/${options.ListeInscriptionGarderieXLSX_Path}`)[0].data
 var ListeInscriptionGarderie = []
 
 for (var i = 1; i < ListeInscriptionGarderieXLSX.length; i++) {
@@ -38,11 +56,11 @@ console.log(`[+] ${ListeInscriptionGarderie.length} élèves inscrits à la gard
 console.log("")
 
 console.log("[+] Récupération des informations de facturation".green)
-var ListeEleveTotalXLSX = xlsx.parse(`${__dirname}/data/Facture 6-8 ans modèle  BE 2022-2023.xlsx`)
+var ListeEleveTotalXLSX = xlsx.parse(`${__dirname}/data/${options.ListeEleveTotalXLSX_Path}`)
 var ListeEleveTotal = []
 var InfoFacturation = {}
-for(var i = 0; i < ListeEleveTotalXLSX.length; i++) {
-  if(isNaN(ListeEleveTotalXLSX[i].name)){
+for (var i = 0; i < ListeEleveTotalXLSX.length; i++) {
+  if (isNaN(ListeEleveTotalXLSX[i].name)) {
     InfoFacturation.Periode = ListeEleveTotalXLSX[i].data[1][1]
     InfoFacturation.Classe = ListeEleveTotalXLSX[i].data[3][1].replace(/[()]/g, "");
     InfoFacturation.TauxHoraire = ListeEleveTotalXLSX[i].data[5][1]
@@ -56,16 +74,16 @@ for(var i = 0; i < ListeEleveTotalXLSX.length; i++) {
     console.log("")
 
     console.log("[+] Récupération du total de chaque élève".green)
-    for(var j = 0; j < ListeEleveTotalXLSX[i].data.length; j++) {
-      if(j > 2){
-        if(!isNaN(ListeEleveTotalXLSX[i].data[j][2])){
+    for (var j = 0; j < ListeEleveTotalXLSX[i].data.length; j++) {
+      if (j > 2) {
+        if (!isNaN(ListeEleveTotalXLSX[i].data[j][2])) {
           var EleveInfo = {
             Nom: ListeEleveTotalXLSX[i].data[j][3].split(",")[0],
-            Prenom: ListeEleveTotalXLSX[i].data[j][3].split(",")[1],
+            Prenom: ListeEleveTotalXLSX[i].data[j][3].split(",")[1].replace(" ", ""),
             Total: ListeEleveTotalXLSX[i].data[j][7],
           }
           InfoFacturation.Total += ListeEleveTotalXLSX[i].data[j][7]
-          if(EleveInfo.Total > 0){
+          if (EleveInfo.Total > 0) {
             InfoFacturation.NbEleve += 1
           }
           ListeEleveTotal.push(EleveInfo)
@@ -75,7 +93,7 @@ for(var i = 0; i < ListeEleveTotalXLSX.length; i++) {
     console.log(`[+] ${InfoFacturation.Total}€ à facturer sur ${InfoFacturation.NbEleve} élèves`.green)
     var Table = new asciiTable(`${InfoFacturation.Periode} (${InfoFacturation.Classe})`)
     Table.setHeading('Nom', 'Prénom', 'Total')
-    for(var j = 0; j < ListeEleveTotal.length; j++) {
+    for (var j = 0; j < ListeEleveTotal.length; j++) {
       Table.addRow(ListeEleveTotal[j].Nom, ListeEleveTotal[j].Prenom, `${ListeEleveTotal[j].Total}€`)
     }
     console.log("")
@@ -85,7 +103,7 @@ for(var i = 0; i < ListeEleveTotalXLSX.length; i++) {
 
 console.log("")
 console.log("[+] Génération des factures".green)
-for(var i = 0; i < ListeEleveTotal.length; i++){
+for (var i = 0; i < ListeEleveTotal.length; i++) {
   var HTML = `
   <html>
     <head>
@@ -136,4 +154,57 @@ for(var i = 0; i < ListeEleveTotal.length; i++){
     type: "",
   }
   pdf.create(document, options)
+  if (i == ListeEleveTotal.length - 1) {
+    console.log(`[+] ${ListeEleveTotal.length} factures générées dans ${__dirname}/factures/${InfoFacturation.Periode}_${InfoFacturation.Classe.replace("/", "-")}/`.green)
+  }
 }
+
+console.log("")
+
+console.log("[+] Récupération des adresses email".green)
+
+for (var i = 0; i < ListeEleveTotal.length; i++) {
+  for (var j = 0; j < ListeInscriptionGarderie.length; j++) {
+    if (ListeEleveTotal[i].Nom == ListeInscriptionGarderie[j].Nom && ListeEleveTotal[i].Prenom == ListeInscriptionGarderie[j].Prenom) {
+      ListeEleveTotal[i].Email1 = ListeInscriptionGarderie[j].Email1
+      ListeEleveTotal[i].Email2 = ListeInscriptionGarderie[j].Email2
+    }
+  }
+}
+console.log(`[+] ${ListeEleveTotal.length} adresses email récupérées`.green)
+
+console.log("")
+
+console.log("[!] Appuyez sur la touche 'Entrée' pour envoyer les factures par email ou appuyez sur 'ctrl'+'c' pour annuler".bgYellow.black.bold)
+
+process.stdin.on('keypress', function (ch, key) {
+  process.stdin.pause();
+  console.log(`[+] Envoie de ${ListeEleveTotal.length} factures par email en cours...`.green)
+  console.log("")
+  for (var i = 0; i < ListeEleveTotal.length; i++) {
+    var MailOptions = {
+      from: 'Le Petit d\'Homme',
+      to: `yanivdouieb2008@gmail.com`,
+      subject: `Facture de garderie des mois de ${InfoFacturation.Periode}`,
+      text: `Bonjour Madame, Monsieur,\n\nVeuillez trouver ci-joint la facture de garderie des mois de ${InfoFacturation.Periode}.\n\nCordialement,\n\nLe Petit d'Homme`,
+      attachments: [
+        {
+          filename: `${ListeEleveTotal[i].Nom}_${ListeEleveTotal[i].Prenom}.pdf`,
+          path: `./factures/${InfoFacturation.Periode}_${InfoFacturation.Classe.replace("/", "-")}/${ListeEleveTotal[i].Nom}_${ListeEleveTotal[i].Prenom}.pdf`,
+          contentType: 'application/pdf'
+        }
+      ]
+    }
+    if (ListeEleveTotal[i].Email1 != "") {
+      transporter.sendMail(MailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(`[+] Facture de ${ListeEleveTotal[i].Nom} ${ListeEleveTotal[i].Prenom} envoyée à ${MailOptions.to}`.green);
+        }
+      })
+    }
+  }
+  console.log("")
+  console.log("Envoie terminé !".green)
+});
